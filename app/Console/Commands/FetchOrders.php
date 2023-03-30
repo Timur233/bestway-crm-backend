b60c67acce4b0d54298bbc89b530b4a9c1287968
Iskandarov Timur <timur.iskandarov96@gmail.com>
2023-03-30 14:09:50 +0600

    test

--- app/Console/Commands/FetchOrders.php
+++ app/Console/Commands/FetchOrders.php
@@ -43,23 +43,23 @@ class FetchOrders extends Command
 
     private function fetchOrdersFromExternalAPI()
     {
-        $client = new Client();
-        $response = $client->request('GET', 'https://kaspi.kz/shop/api/v2/orders', [
-            'headers' => [
-                'Content-Type' => 'application/vnd.api+json',
-                'X-Auth-Token' => 'v5fgjD5Y2v7++RytwB2RV0ndMqBbVgSpAaE/EytLwgw='
-            ],
-            'query' => [
-                'page[number]' => '0',
-                'page[size]' => '1000',
-                'filter[orders][state]' => 'ARCHIVE',
-                'filter[orders][creationDate][$ge]' => '1679881434000',
-                'filter[orders][creationDate][$le]' => '1680054234000',
-            ]
-        ]);
+        // $client = new Client();
+        // $response = $client->request('GET', 'https://kaspi.kz/shop/api/v2/orders', [
+        //     'headers' => [
+        //         'Content-Type' => 'application/vnd.api+json',
+        //         'X-Auth-Token' => 'v5fgjD5Y2v7++RytwB2RV0ndMqBbVgSpAaE/EytLwgw='
+        //     ],
+        //     'query' => [
+        //         'page[number]' => '0',
+        //         'page[size]' => '1000',
+        //         'filter[orders][state]' => 'ARCHIVE',
+        //         'filter[orders][creationDate][$ge]' => '1679881434000',
+        //         'filter[orders][creationDate][$le]' => '1680054234000',
+        //     ]
+        // ]);
 
-        $body = $response->getBody();
-        $content = $body->getContents();
+        // $body = $response->getBody();
+        $content = '$body->getContents()';
 
         Log::error('str #56 ' . $content);
 
